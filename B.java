23e093ca29883800c14d0f4ea306c042c70861dd
Ricardo Caldeira <ricardo_tedo@hotmail.com>
2018-10-24 16:49:18 -0300

    String b4 adicionada a classe B

--- B.java
+++ B.java
@@ -4,22 +4,13 @@ package gestaoversoes;
  */
 
 public class B {
-    private int b1;
-    private float b2;
+    private String b4;
     
-    public int getB1() {
-        return b1;
+    public String getB4() {
+        return b4;
     }
     
-    public void setB1(int b1) {
-        this.b1 = b1;
-    }
-
-    public float getB2() {
-        return b2;
-    }
-
-    public void setB2(float b2) {
-        this.b2 = b2;
+    public void setB4(String b4) {
+        this.b4 = b4;
     }
 }
