467c12a23325f02870b226c07e2448ac442edea1
Ricardo Caldeira <ricardo_tedo@hotmail.com>
2018-10-24 16:23:31 -0300

    String Aramo4 adicionada no branch ramo1

--- A.java
+++ A.java
@@ -7,6 +7,7 @@ public class A {
     private int a1;
     private float a2;
     private String a3;
+    private String a_ramo4;
 
     public int getA1() {
         return a1;
@@ -24,12 +25,20 @@ public class A {
         this.a2 = a2;
     }
     
-     public String getA3() {
+    public String getA3() {
         return a3;
     }
 
     public void setA3(String a3) {
         this.a3 = a3;
+    }
+    
+    public String getA_ramo4() {
+        return a_ramo4;
+    }
+
+    public void setA_ramo4(String a_ramo4) {
+        this.a_ramo4 = a_ramo4;
     }
     
     public float soma(){
